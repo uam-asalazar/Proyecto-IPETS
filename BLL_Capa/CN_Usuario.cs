e7dfc7d3467d2ffefdaed18557c7cb69b0df814b
uam-asalazar <161781520+uam-asalazar@users.noreply.github.com>
2024-03-10 21:28:27 -0600

    Add files via upload

---
+++ BLL_Capa/CN_Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaEntidad;
+using DLL_Capa;
+
+namespace BLL_Capa
+{
+    public class CN_Usuario
+    {
+
+        private CD_Usuario objcd_usuario = new CD_Usuario();
+
+        public List<Usuario> Listar()
+        {
+            return objcd_usuario.Listar();
+        }
+
+    }
+}
---
+++ DLL_Capa/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DLL_Capa
+{
+    public class Conexion
+    {
+        public static string cadena = ConfigurationManager.ConnectionStrings["cadena_conexion"].ToString();
+    }
+}
